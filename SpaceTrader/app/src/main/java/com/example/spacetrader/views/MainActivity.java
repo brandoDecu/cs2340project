6005815554d13180cb2f21637d48feb74eb27cd6
dvogel3 <dvogel3@gatech.edu>
2019-02-13 08:18:38 -0500

    Created file directories for entity, model, viewmodels, and views. Additionally changed package names and imported classes for viable application build.

---
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.spacetrader.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.spacetrader.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/edu/gatech/space_trader/views/MainActivity.java
+++ app/src/main/java/edu/gatech/space_trader/views/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.spacetrader;
+package edu.gatech.space_trader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
