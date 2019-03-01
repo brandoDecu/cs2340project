2661a49f2d1318cee01cef540a5f012d1d04cbed
brandoDecu <brandodecourten@gmail.com>
2019-03-01 17:50:39 -0500

    setter for marketplace

--- SpaceTrader/app/src/main/java/com/theLads/spacetrader/entity/MarketPlace.java
+++ SpaceTrader/app/src/main/java/com/theLads/spacetrader/entity/MarketPlace.java
@@ -4,29 +4,41 @@ import com.theLads.spacetrader.entity.enums.ItemType;
 import com.theLads.spacetrader.entity.enums.Resources;
 import com.theLads.spacetrader.entity.enums.TechLevel;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MarketPlace {
 
 
 
-    Map<ItemType, Integer> itemQuantity = new HashMap<>();
+    private List<Integer> itemQuantity = new ArrayList<>();
 
-    Map<ItemType, Integer> itemPrice = new HashMap<>();
+    private List<Double> itemPrice = new ArrayList<>();
 
     public MarketPlace(TechLevel techLvl, Resources resources) {
         for(ItemType item : ItemType.values()) {
-            itemQuantity.put(item, 10);
-            itemPrice.put(item,20);
+            itemQuantity.add(50);
+            itemPrice.add(9.99);
         }
     }
 
-    public Map<ItemType, Integer> getItemQuantity() {
+    public List<ItemType> getItems() {
+        return new ArrayList<ItemType>(Arrays.asList(ItemType.values()));
+    }
+
+    public List<Integer> getItemQuantity() {
         return itemQuantity;
     }
 
-    public Map<ItemType, Integer> getItemPrice() {
+    public List<Double> getItemPrice() {
         return itemPrice;
+    }
+
+    public void setItemQuantity(ItemType item, int quantity) {
+        int index = getItems().indexOf(item);
+        itemQuantity.set(index, quantity);
     }
 }
