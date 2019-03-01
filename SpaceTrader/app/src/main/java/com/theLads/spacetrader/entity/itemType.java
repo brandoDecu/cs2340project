03aec180423b646c1c47f1cd3f64dcd14435e582
dvogel3 <dvogel3@gatech.edu>
2019-03-01 09:51:40 -0500

    Added marketplace and item classes

---
+++ SpaceTrader/app/src/main/java/com/theLads/spacetrader/entity/itemType.java
@@ -0,0 +1,10 @@
+package com.theLads.spacetrader.entity;
+
+/**
+ * Enum class for type of items for the marketplace
+ */
+
+public enum itemType {
+
+    WATER, FURS, FOOD, ORE, GAMES, FIREARMS, MEDICINE, MACHINES, NARCOTICS, ROBOTS;
+}
---
+++ SpaceTrader/app/src/main/java/com/theLads/spacetrader/entity/marketPlace.java
@@ -0,0 +1,18 @@
+package com.theLads.spacetrader.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class marketPlace {
+
+    Planet planet;
+
+    Map<String, Integer> itemQuantityList = new HashMap<>();
+
+    Map<String, Integer> itemPrices = new HashMap<>();
+
+
+
+
+
+}
