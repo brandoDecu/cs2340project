d18abdeb776950e49d277a5c123af2c930d981dc
JoshMJ37 <jmjaffe37@gatech.edu>
2019-03-04 20:51:34 -0500

    started making buy_detail_view

--- SpaceTrader/app/src/main/java/com/theLads/spacetrader/views/BuyDetailActivity.java
+++ SpaceTrader/app/src/main/java/com/theLads/spacetrader/views/BuyDetailActivity.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.Nullable;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.theLads.spacetrader.R;
+import com.theLads.spacetrader.entity.itemType;
 
 public class BuyDetailActivity extends AppCompatActivity {
+
+    private TextView itemType;
+    private TextView itemQuant;
+    private EditText quatityField;
+
+    private com.theLads.spacetrader.entity.itemType item;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new);
+        setContentView(R.layout.activity_buy_detail);
 
+        quatityField = findViewById(R.id.nameField);
+//        item = (itemType) getIntent().item();
+//        Intent i = new Intent(this, NewActivity.class);
+//        this.startActivity(i);
+//        this.finish();
     }
 }
