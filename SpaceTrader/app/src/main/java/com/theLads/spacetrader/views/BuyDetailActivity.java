bd10f55a49125e7758d04ea933efac4d64d94b52
JoshMJ37 <jmjaffe37@gatech.edu>
2019-03-06 14:41:34 -0500

    Continued to work on buy_detail_activity, added background to buy

--- SpaceTrader/app/src/main/java/com/theLads/spacetrader/views/BuyDetailActivity.java
+++ SpaceTrader/app/src/main/java/com/theLads/spacetrader/views/BuyDetailActivity.java
@@ -1,22 +1,36 @@
 package com.theLads.spacetrader.views;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.theLads.spacetrader.R;
-import com.theLads.spacetrader.entity.itemType;
+import com.theLads.spacetrader.entity.enums.ItemType;
+import com.theLads.spacetrader.model.Model;
+import com.theLads.spacetrader.viewmodels.BuySellViewModel;
 
 public class BuyDetailActivity extends AppCompatActivity {
 
+    private BuySellViewModel viewModel;
+
     private TextView itemType;
     private TextView itemQuant;
+    private TextView priceTag;
+    private TextView creditsTag;
     private EditText quatityField;
 
-    private com.theLads.spacetrader.entity.itemType item;
+    private ItemType item;
+
+    private int quantity;
+    private double price;
+    private int supply;
+
 
 
     @Override
@@ -24,10 +38,36 @@ public class BuyDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_detail);
 
-        quatityField = findViewById(R.id.nameField);
-//        item = (itemType) getIntent().item();
-//        Intent i = new Intent(this, NewActivity.class);
-//        this.startActivity(i);
-//        this.finish();
+        quatityField = findViewById(R.id.quatityField);
+        itemQuant = findViewById(R.id.itemQuant);
+        priceTag = findViewById(R.id.priceTag);
+        creditsTag = findViewById(R.id.creditsTag);;
+
+
+        supply = Model.getInstance().getGameInteractor().getMarketQuantities().get(0);
+        price = Model.getInstance().getGameInteractor().getMarketPrices().get(0);
+        Double credits = Model.getInstance().getGameInteractor().getCredits();
+
+        itemQuant.setText(supply);
+        priceTag.setText(String.format("%.2f", price));
+        creditsTag.setText(String.format("%.2f", credits));
+
+        viewModel = ViewModelProviders.of(this).get(BuySellViewModel.class);
+
+    }
+
+    public void onAddPressed(View view) {
+
+        quantity = Integer.parseInt(quatityField.getText().toString());
+        if (quantity <= supply) {
+            viewModel.buyItem(ItemType.WATER, quantity, price);
+            Toast.makeText(this, String.format("%f items baught", quantity), Toast.LENGTH_LONG).show();
+
+            Intent i = new Intent(this, BuyActivity.class);
+            this.startActivity(i);
+            this.finish();
+        } else {
+            Toast.makeText(this, String.format("This store only has %f of this item", supply), Toast.LENGTH_LONG).show();
+        }
     }
 }
