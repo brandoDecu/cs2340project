3707d46111b47f5fec032b569dc0fd229893e767
brandoDecu <43319223+brandoDecu@users.noreply.github.com>
2019-03-29 16:06:09 -0400

    Revert "fixed bug for log in"

--- SpaceTrader/app/src/main/java/com/theLads/spacetrader/model/GameInteractor.java
+++ SpaceTrader/app/src/main/java/com/theLads/spacetrader/model/GameInteractor.java
@@ -27,13 +27,7 @@ public class GameInteractor extends Interactor {
         return getRepository().getCurrentGame();
     }
 
-    public String getPlanetName() {
-        if (getCurrentGame() == null) {
-            Log.d("yolo", "ylol");
-        }
-
-        return getCurrentGame().getPlanetName();
-    }
+    public String getPlanetName() {return getCurrentGame().getPlanetName();}
 
     public void setCredits(double credits) {
         Player player = getCurrentGame().getPlayer();
