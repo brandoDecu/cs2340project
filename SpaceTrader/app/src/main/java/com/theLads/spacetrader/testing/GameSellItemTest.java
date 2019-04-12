0f83603fdbb4bf5aac20c672413f51b62245502e
jrajakumar3 <jrajakumar3@gatech.edu>
2019-04-11 22:59:21 -0400

    junit for sellitem

---
+++ SpaceTrader/app/src/main/java/com/theLads/spacetrader/testing/GameSellItemTest.java
@@ -0,0 +1,48 @@
+package com.theLads.spacetrader.testing;
+
+import com.theLads.spacetrader.entity.Galaxy;
+import com.theLads.spacetrader.entity.Game;
+import com.theLads.spacetrader.entity.Player;
+import com.theLads.spacetrader.entity.Ship;
+import com.theLads.spacetrader.entity.enums.GameDifficulty;
+import com.theLads.spacetrader.entity.enums.ItemType;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
+public class GameSellItemTest {
+
+    @Test
+    public final void sellItem() {
+        Player testPlayer = new Player("test", 4,4,4,4);
+        Galaxy testGalaxy = new Galaxy();
+        Game testGame = new Game(GameDifficulty.EASY, testPlayer, testGalaxy);
+
+        try {
+            testGame.sellItem(ItemType.WATER, 1, ItemType.WATER.getBasePrice());
+            fail("Expected an exception to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), is("Cargo hold is full"));
+        }
+    }
+
+    @Test
+    public final void sellWithFullCargo(){
+        Player testPlayer = new Player("test", 4,4,4,4);
+        Galaxy testGalaxy = new Galaxy();
+        Game testGame = new Game(GameDifficulty.EASY, testPlayer, testGalaxy);
+        Ship testShip = testPlayer.getShip();
+        testShip.setItemQuantity(ItemType.WATER, 2);
+
+        testGame.sellItem(ItemType.WATER, 1, ItemType.WATER.getBasePrice());
+
+        Assert.assertEquals(1020, (int) testPlayer.getCredits());
+        Assert.assertEquals(1, (int) testPlayer.getShip().getQuantityOf(ItemType.WATER));
+    }
+
+
+}
